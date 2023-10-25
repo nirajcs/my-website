dc812f0dcada5573b345dd1771a0affefa28ee92
Niraj <ncs7498@gmail.com>
2023-10-25 08:59:29 +0530

    imaage route corrected

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,35 +1,35 @@
 export const projectsData = [
     {
         name:'Medicare',
-        image:'/public/project-images/medicare.png',
+        image:'/project-images/medicare.png',
         git:'https://github.com/nirajcs/medicare',
         technologies:['ReactJs','Redux','Json Web Token','ExpressJS','NodeJS','MongoDB','Tailwind CSS','Socket.IO','Nodemailer','Multer','Datatables','React-ChartJS','Stripe Payment','AWS','Nginx'],
         description:"Medicare is a web application for booking doctor appointments,The application consists of User,Doctor and Admin sides."
     },
     {
         name:'DC Wears',
-        image:'/public/project-images/dcwears.png',
+        image:'/project-images/dcwears.png',
         git:'https://github.com/nirajcs/dcwears',
         technologies:['ExpressJS','NodeJS','MongoDB','Bootstrap','Handlebars','Nodemailer','Multer','Datatables','ChartJS','Razorpay Payment','AWS','Nginx'],
         description:"DC Wears is an E-commerce website for selling and purchasing clothings related to superheros of DC universe."
     },
     {
         name:'Netflix-clone',
-        image:'/public/project-images/netflix.png',
+        image:'/project-images/netflix.png',
         technologies:['TMDB API','React-youtube','ReactJS'],
         git:'https://github.com/nirajcs/netflix-clone',
         description:'Netflix-clone is a miniproject in which the frontend design of netflix application is built.'
     },
     {
         name:'OLX-clone',
-        image:'/public/project-images/olx.png',
+        image:'/project-images/olx.png',
         technologies:['Firebase','ReactJS'],
         git:'https://github.com/nirajcs/olx-clone',
         description:'OLX Clone is a miniproject in which the design and functionalities of OLX is cloned.The user can add new products for selling.'
     },
     {
         name:'Mern-Auth',
-        image:'/public/project-images/mernauth.png',
+        image:'/project-images/mernauth.png',
         technologies:['Redux','ReactJS','MongoDB','ExpressJS','NodeJS'],
         git:'https://github.com/nirajcs/MernAuth',
         description:'Mern-Auth is a miniproject in which redux is used for user authentication.'
