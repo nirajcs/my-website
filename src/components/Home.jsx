76a335a68411fd92889a669774880623c5fd3161
Niraj <ncs7498@gmail.com>
2023-11-04 16:58:17 +0530

    Dynamic Text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import myimage from '../assets/niraj.jpg'
 import cv from '../assets/Niraj C S-Resume.pdf'
 
 const Home = () => {
+  const [text,setText] = useState('')
+  const [charIndex, setCharIndex] = useState(0);
+  const content = "Self-driven and self-learned, I've mastered the MERN stack through determination and endless curiosity, making me a versatile developer ready to take on any challenge."
+
+  useEffect(() => {
+    const typingTimer = setTimeout(() => {
+      if (charIndex < content.length) {
+        setText(content.substring(0, charIndex + 1));
+        setCharIndex(charIndex + 1);
+      }
+    }, 50); // Adjust the typing speed (e.g., 100ms per character)
+
+    return () => clearTimeout(typingTimer);
+  }, [charIndex, content]);
+
   return (
     <>
-      <div className='md:w-1/2 order-2 text-center md:text-left md:order-1 flex justify-center items-center md:pb-32'>
-        <div className='flex justify-center md:justify-end'>
-          <div className='w-2/3'>
-            <h1 className='hidden md:block text-white text-4xl'>Hi, I am</h1>
-            <h1 className='text-white text-5xl font-bold'>Niraj C S</h1>
-            <h3 className='text-white text-2xl md:my-10'>MERN Stack Developer</h3>
-            <p className='text-white w-full mt-5 md:mt-0'>"Self-driven and self-learned, I've mastered the MERN stack through determination and endless curiosity, making me a versatile developer ready to take on any challenge."</p>
+      <div className='w-full md:w-1/2 order-2 text-center md:text-left md:order-1 flex flex-col justify-center items-center md:pb-40'>
+        <div className='w-5/6 flex justify-center md:justify-start'>
+          <div className='md:ps-10 w-[500px] md:w-full'>
+            <div className="w-full">
+              <h1 className='hidden md:block text-white text-4xl w-full'>Hi, I am</h1>
+            </div>
+            <div className="w-full">
+              <h1 className='text-white text-5xl font-bold w-full'>Niraj C S</h1>
+            </div>
+            <div className='w-full'>
+              <h3 className='text-white text-2xl md:my-10 w-full'>MERN Stack Developer</h3>
+            </div>
           </div>
+        </div>
+        <div className='flex w-5/6 h-[150px] justify-start md:ps-10'>
+          <p className='text-white mt-5 md:mt-0'>{text}</p>
         </div>
       </div>
       <div className='md:w-1/2 order-1 md:order-1 w-full flex flex-col justify-center items-center mb-10 md:mb-0 md:pb-32'>
